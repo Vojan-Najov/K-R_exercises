f0aa2eafc1bb8bf0b55c5072b5f6f1c4df7c69d6
Vojan-Najov <serejah.mashichev@gmail.com>
2024-01-20 22:59:52 +0300

    Add rotating square

---
+++ opengl_samples/rotating_square/rotating_square.c
@@ -0,0 +1,81 @@
+#include <GL/glut.h>
+
+GLfloat spin = 0.0;
+
+static void init(void);
+static void display(void);
+static void spinDisplay(void);
+static void reshape(int w, int h);
+static void mouse(int button, int state, int x, int y);
+
+int main(int argc, char **argv)
+{
+  glutInit(&argc, argv);
+  glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+  glutInitWindowSize(250, 250);
+  glutInitWindowPosition(100, 100);
+  glutCreateWindow("Double buffered");
+  init();
+  glutDisplayFunc(display);
+  glutReshapeFunc(reshape);
+  glutMouseFunc(mouse);
+  glutMainLoop();
+
+  return 0;
+}
+
+static void init(void)
+{
+  glClearColor(0.0, 0.0, 0.0, 0.0);
+  glShadeModel(GL_FLAT);
+}
+
+static void display(void)
+{
+  glClear(GL_COLOR_BUFFER_BIT);
+  glPushMatrix();
+  glRotatef(spin, 0.0, 0.0, 1.0);
+  glColor3f(1.0, 1.0, 1.0);
+  glRectf(-25.0, -25.0, 25.0, 25.0);
+  glPopMatrix();
+  glutSwapBuffers();
+}
+
+static void spinDisplay(void)
+{
+  spin = spin + 1.0;
+
+  if (spin > 360.0) {
+    spin = spin - 360.0;
+  }
+
+  glutPostRedisplay();
+}
+
+static void reshape(int w, int h)
+{
+  //glViewport(0, 0, (GLsizei) w, (GLsizei) h);
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  glOrtho(-50.0, 50.0, -50.0, 50.0, -1.0, 1.0);
+  glMatrixMode(GL_MODELVIEW);
+  glLoadIdentity();
+}
+
+static void mouse(int button, int state, int x, int y)
+{
+  switch (button) {
+    case GLUT_LEFT_BUTTON:
+      if (state == GLUT_DOWN) {
+        glutIdleFunc(spinDisplay);
+      }
+      break;
+    case GLUT_RIGHT_BUTTON:
+      if (state == GLUT_DOWN) {
+        glutIdleFunc(NULL);
+      }
+      break;
+  }
+}
+
+
